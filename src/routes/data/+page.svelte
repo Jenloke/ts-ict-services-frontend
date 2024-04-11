f0a1d07841193c4a17b5b900606e3558cad1fa6a
Araniala <zoomedin277@gmail.com>
2024-04-11 14:42:52 +0800

    migrate table to data table

--- src/routes/data/+page.svelte
+++ src/routes/data/+page.svelte
@@ -1,11 +1,173 @@
 <script lang="ts">
   import type { PageData } from "./$types";
+  import { createTable, Render, Subscribe, createRender } from "svelte-headless-table";
+  import { addPagination, addSortBy, addTableFilter, addHiddenColumns, } from "svelte-headless-table/plugins";
+  import { readable } from "svelte/store";
+  import ChevronDown from "lucide-svelte/icons/chevron-down";
   import * as Table from "$lib/components/ui/table/index.js";
- 
+  import DataTableActions from "./data-table-actions.svelte";
+  import { Button } from "$lib/components/ui/button";
+  import { Input } from "$lib/components/ui/input";
+  import * as DropdownMenu from "$lib/components/ui/dropdown-menu";
+  
   export let data: PageData;
   const Equipment = data.equipments
+  
+  const table = createTable(readable(Equipment),{
+    page: addPagination(),
+    sort: addSortBy(),
+    filter: addTableFilter({
+      fn: ({ filterValue, value }) =>
+        value.toLowerCase().includes(filterValue.toLowerCase()),
+    }),
+    hide: addHiddenColumns(),
+  });
+
+  const columns = table.createColumns([
+    table.column({
+      accessor: "equipmentID",
+      header: "Equipment ID",
+    }),
+    table.column({
+      accessor: "issuedTo",
+      header: "Issued To",
+    }),
+    table.column({
+      accessor: "condition",
+      header: "Condition",
+    }),
+    table.column({
+      accessor: "location",
+      header: "Location",
+    }),
+    table.column({
+      accessor: "numOfUnits",
+      header: "Number of Units",
+    }),
+    table.column({
+      accessor: "remarks",
+      header: "Remarks",
+      plugins: {
+        filter: {
+          exclude: true,
+        },
+      },
+    }),
+    table.column({
+      accessor: "status",
+      header: "Status",
+    }),
+    table.column({
+      accessor: "usageRate",
+      header: "Usage Rate",
+      plugins: {
+        filter: {
+          exclude: true,
+        },
+      },
+    }),
+    table.column({
+      accessor: ({ equipmentID }) => equipmentID,
+      header: "",
+      cell: ({ value }) => {
+        return createRender(DataTableActions, { id: value });
+      },
+    }),
+  ]);
+
+  const { headerRows, pageRows, tableAttrs, tableBodyAttrs, pluginStates, flatColumns, } =
+    table.createViewModel(columns);
+
+  const { hasNextPage, hasPreviousPage, pageIndex } = pluginStates.page;
+  const { filterValue } = pluginStates.filter;
+  const { hiddenColumnIds } = pluginStates.hide;
+
+  const ids = flatColumns.map((col) => col.id);
+  let hideForId = Object.fromEntries(ids.map((equipmentid) => [equipmentid, true]));
+
+  $: $hiddenColumnIds = Object.entries(hideForId)
+    .filter(([, hide]) => !hide)
+    .map(([equipmentid]) => equipmentid);
+ 
+  const hidableCols = ["numOfUnits", "remarks", "usageRate"];
 </script>
 
+<div>
+  <div class="flex items-center py-4">
+    <Input
+      class="max-w-sm"
+      placeholder="Filter equipment..."
+      type="text"
+      bind:value={$filterValue}
+    />
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild let:builder>
+        <Button variant="outline" class="ml-auto" builders={[builder]}>
+          Columns <ChevronDown class="ml-2 h-4 w-4" />
+        </Button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        {#each flatColumns as col}
+          {#if hidableCols.includes(col.id)}
+            <DropdownMenu.CheckboxItem bind:checked={hideForId[col.id]}>
+              {col.header}
+            </DropdownMenu.CheckboxItem>
+          {/if}
+        {/each}
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  </div>
+  <div class="rounded-md border">
+    <Table.Root {...$tableAttrs}>
+      <Table.Header>
+        {#each $headerRows as headerRow}
+          <Subscribe rowAttrs={headerRow.attrs()}>
+            <Table.Row>
+              {#each headerRow.cells as cell (cell.id)}
+                <Subscribe attrs={cell.attrs()} let:attrs props={cell.props()}>
+                  <Table.Head {...attrs}>
+                    <Render of={cell.render()} />
+                  </Table.Head>
+                </Subscribe>
+              {/each}
+            </Table.Row>
+          </Subscribe>
+        {/each}
+      </Table.Header>
+      <Table.Body {...$tableBodyAttrs}>
+        {#each $pageRows as row (row.id)}
+          <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+            <Table.Row {...rowAttrs}>
+              {#each row.cells as cell (cell.id)}
+                <Subscribe attrs={cell.attrs()} let:attrs>
+                  <Table.Cell {...attrs}>
+                    <Render of={cell.render()} />
+                  </Table.Cell>
+                </Subscribe>
+              {/each}
+            </Table.Row>
+          </Subscribe>
+        {/each}
+      </Table.Body>
+    </Table.Root>
+  </div>
+  <div class="flex items-center justify-end space-x-4 py-4">
+    <Button
+      variant="outline"
+      size="sm"
+      on:click={() => ($pageIndex = $pageIndex - 1)}
+      disabled={!$hasPreviousPage}>Previous</Button
+    >
+    <Button
+      variant="outline"
+      size="sm"
+      disabled={!$hasNextPage}
+      on:click={() => ($pageIndex = $pageIndex + 1)}>Next</Button
+    >
+  </div>
+</div>
+
+<!--
 <div class="container">
 <Table.Root class="w-900 bg-gray-500/10 border-spacing-2 border border-slate-500">
   <Table.Caption>A list of ICT Equipments</Table.Caption>
@@ -37,3 +199,4 @@
   </Table.Body>
 </Table.Root>
 </div>
+-->
---
+++ src/routes/data/data-table-actions.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+    import Ellipsis from "lucide-svelte/icons/ellipsis";
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+    import { Button } from "$lib/components/ui/button";
+   
+    export let id: string;
+  </script>
+   
+  <DropdownMenu.Root>
+    <DropdownMenu.Trigger asChild let:builder>
+      <Button
+        variant="ghost"
+        builders={[builder]}
+        size="icon"
+        class="relative h-8 w-8 p-0"
+      >
+        <span class="sr-only">Open menu</span>
+        <Ellipsis class="h-4 w-4" />
+      </Button>
+    </DropdownMenu.Trigger>
+    <DropdownMenu.Content>
+      <DropdownMenu.Group>
+        <DropdownMenu.Label>Actions</DropdownMenu.Label>
+        <DropdownMenu.Item on:click={() => navigator.clipboard.writeText(id)}>
+          Copy equipment ID
+        </DropdownMenu.Item>
+      </DropdownMenu.Group>
+    </DropdownMenu.Content>
+  </DropdownMenu.Root>
